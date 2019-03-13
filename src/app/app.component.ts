5b79a85d75edb8bdf435e37b1447b2d2d6fe9fca
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-03-13 14:56:28 +0200

    add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,15 +21,15 @@ export class AppComponent implements OnInit {
     this.todoList = new TodoList();
 
     const project1 = new Project(1, 'Default');
-    project1.addTask(new Task(1, 'First task #1', false));
-    project1.addTask(new Task(2, 'Second task #2', false));
-    project1.addTask(new Task(3, 'Third task #3 - checked', true));
+    project1.addTask(new Task(1, 'Default - First task #1', false));
+    project1.addTask(new Task(2, 'Default - Second task #2', false));
+    project1.addTask(new Task(3, 'Default - Third task #3 - checked', true));
     this.todoList.addProject(project1);
 
-    const project2 = new Project(2, 'Default');
-    project2.addTask(new Task(4, 'First task #4', false));
-    project2.addTask(new Task(5, 'Second task #5', false));
-    project2.addTask(new Task(6, 'Third task #6 - checked', true));
+    const project2 = new Project(2, 'Other');
+    project2.addTask(new Task(4, 'Other - First task - #4', false));
+    project2.addTask(new Task(5, 'Other - Second task #5', false));
+    project2.addTask(new Task(6, 'Other - Third task #6 - checked', true));
     this.todoList.addProject(project2);
 
   }
