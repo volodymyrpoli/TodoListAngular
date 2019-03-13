ec92b3c0519b119e7fba4c0385657b341646494e
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-03-13 12:57:05 +0200

    create task list item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { OneFieldFormComponent } from './components/one-field-form/one-field-form.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ProjectListItemComponent } from './components/project-list-item/project-list-item.component';
+import { TaskListItemComponent } from './components/task-list-item/task-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OneFieldFormComponent,
     ProjectListItemComponent,
+    TaskListItemComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/task-list-item/task-list-item.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Task} from '../../entities/Task';
+
+@Component({
+  selector: 'app-task-list-item',
+  templateUrl: './task-list-item.component.html',
+  styleUrls: ['./task-list-item.component.css']
+})
+export class TaskListItemComponent implements OnInit {
+
+  @Input() task: Task;
+  @Input() removeButtonTitle: string;
+  @Output() removeProject = new EventEmitter<Task>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  remove(event) {
+    this.removeProject.emit(this.task);
+  }
+
+}
